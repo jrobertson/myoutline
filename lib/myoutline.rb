293282b8848f24fc647b5f4c9c1d1678c6ebc040
Your Name <you@example.com>
2018-03-18 12:57:41 +0000

    feature: The outline file being read will now be updated automatically by default to include the alphabetical index.

--- lib/myoutline.rb
+++ lib/myoutline.rb
@@ -7,14 +7,21 @@ require 'pxindex'
 
 class MyOutline
 
-  def initialize(raw_s, debug: false, allsorted: true)
+  def initialize(source, debug: false, allsorted: true, autoupdate: true)
+    
+    @debug, @source = debug, source
 
+    raw_s, _ = RXFHelper.read(source)
+    
     # find the entries which aren't on the main index
     s = raw_s.sub(/<[^>]+>\n/,'')
     doc = LineTree.new(s, debug: true).to_doc(encapsulate: true)
     a = doc.root.xpath('entry/text()')
+    puts 'doc: ' + doc.xml if debug
     a2 = doc.root.xpath('entry//entry/text()')
+    puts 'a2: ' + a2.inspect if debug
     a3 = a2 - a
+    puts 'a3:' + a3.inspect if debug
     
     # add the new entries to the main index
     s << a3.join("\n")
@@ -25,16 +32,38 @@ class MyOutline
     
     @pxi = PxIndex.new(s, debug: debug, indexsorted: true, 
                        allsorted: allsorted)
-    @px = @pxi.to_px
+    save() if autoupdate and self.to_s != raw_s
 
   end
   
-  def save(filename=myoutline.txt)
-    File.write filename, @pxi.to_s
+  def save(filename=nil)
+    
+    if filename.nil? then
+      filename = RXFHelper.writeable?(@source) ? @source : 'myoutline.txt'
+    end
+  
+    RXFHelper.write filename, self.to_s
+    
   end
   
-  def to_s()
-    @pxi.to_s
+  def to_s(declaration: true)
+    
+    if declaration == true then
+      @pxi.to_s.sub(/(?<=^\<\?)([^\?]+)/,'myoutline')
+    else
+      @pxi.to_s.lines[1..-1].join.lstrip
+    end
+    
+  end
+  
+  def to_tree
+    
+    a  = @pxi.to_s.lines
+    a.shift # remove the ph declaration
+    a.reject! {|x| x =~ /^(?:#[^\n]+|\n+)$/}
+    
+    a.join
+    
   end
 
 end
--- myoutline.gemspec
+++ myoutline.gemspec
@@ -1,10 +1,10 @@
 Gem::Specification.new do |s|
   s.name = 'myoutline'
-  s.version = '0.1.1'
+  s.version = '0.1.2'
   s.summary = 'Helps build an outline from plain text.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/myoutline.rb']
-  s.add_runtime_dependency('pxindex', '~> 0.1', '>=0.1.4')
+  s.add_runtime_dependency('pxindex', '~> 0.1', '>=0.1.6')
   s.signing_key = '../privatekeys/myoutline.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
